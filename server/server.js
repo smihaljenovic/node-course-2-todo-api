4719d6c9ec1e6c5d302c71d8efa38c1f5c1d4eb5
Saša Mihaljenović <smihaljenovic@gmail.com>
2018-02-20 20:03:28 +0100

    tests

--- server/server.js
+++ server/server.js
@@ -18,7 +18,15 @@ app.post('/todos', (req, res) => {
     todo.save().then(doc => {
         res.send(doc);
     }, e => {
-       res.send(e);
+       res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then(todos => {
+        res.send({todos});
+    }, e => {
+        res.status(400).send(e);
     });
 });
 
